03fd329e0bf0bcca3a99eef5375e8e968bf35769
DanteOnline <iamdanteonline@gmail.com>
2019-07-26 12:48:07 +0300

    add functions

---
+++ main.py
@@ -0,0 +1,28 @@
+"""Функции для тренировки написания тестов на pytest"""
+
+
+def get_positive_numbers(all_numbers):
+    """
+    :param all_numbers: список любых чисел [1, 3, -4, -10, 3 ... ]
+    :return: список только положительных чисел
+    """
+    pass
+
+
+def get_pi_number(sign_count):
+    """
+    :param sign_count: количество знаков после запятой
+    :return: число ПИ округленное до переданного количество знаков
+    """
+    pass
+
+
+def cool_text(text, words_count=1, sep='*'):
+    """
+    Красивый вывод текста
+    :param text: слово которое выводим
+    :param sep: разделетиель
+    :param words_count: количество слов
+    :return: красивый текст
+    """
+    pass
